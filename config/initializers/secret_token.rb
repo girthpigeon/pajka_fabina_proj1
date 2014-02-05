9ef9fc45895a6b6d3739d4fe479b09cc64e7c75b
Zack <admin@Admins-Mac.local>
2014-02-05 15:49:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PajkaFabinaProj1::Application.config.secret_token = '5ac1df4eca91eb7e545fccd51f79961a6b4c825601eb43bff29112936beb53623c21c6e8752330da4a07901e3b7c96cc6f90f8a8496238deb157a89257327bb6'
